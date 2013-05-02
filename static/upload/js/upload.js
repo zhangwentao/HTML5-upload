2e9f0594a965b29e684ef62d4fe944e1d3ef78c0
zhangwentao <taowenzhang@gmail.com>
2013-05-02 10:50:35 +0800

    save

--- static/upload/js/upload.js
+++ static/upload/js/upload.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
 	var fileInput = $('#pics-input');		
 	var thumbCon = $('#thumb-container');
+	var uploadList = new UploadItemList();
+	var idGen = new UploadIdGen('upload-pic-');
+	var key = $('input[type="hidden"]')[0];
+	console.log(key);
 	fileInput.on('change',handleFileInputChange);
-
+	$('#upload-btn').on('click',handleUploadBtnClick);	
 	function adjust(img,w,h) {
 		var originRatio = img.width / img.height;
 		if (originRatio >= w / h) {
@@ -21,9 +25,11 @@ $(document).ready(function(){
 	function renderPicFiles(fileList) {
 		$.each(fileList,function(index,file) {
 			console.log(file);
+			var fileItem = new UploadItem(file,idGen.gen());
+			uploadList.push(fileItem);
 			var fileReader = new FileReader();
 			fileReader.onload = function(evt) {
-				var thumb = $('<div class="thumb"></div>');				
+				var thumb = $('<div class="thumb"><div class="close-btn" upload-id="'+fileItem.id+'">x</div><progress></progress></div>');				
 				var img = $('<img/>');
 				thumb.append(img);
 				thumbCon.append(thumb);
@@ -35,7 +41,35 @@ $(document).ready(function(){
 			fileReader.readAsDataURL(file);
 		});
 	}
-	
+
+	function handleUploadBtnClick(evt) {
+		for(var i = 0; i < uploadList.length; i++) {
+			uploadFile(uploadList[i]);
+		}
+	}
+
+	function uploadFile(fileItem) {
+		var req = new XMLHttpRequest();
+		req.upload.onprogress = function(evt) {
+			var progressbar = $('[upload-id="'+fileItem.id+'"]').siblings('progress')[0];
+			console.log(progressbar);
+			progressbar.max = evt.total;
+			progressbar.value = evt.position;
+		}
+		req.open('POST','/upload/upload/');
+		var data = new FormData();
+		data.append(key.name,key.value);
+		data.append(fileItem.id,fileItem.file);
+		req.send(data);
+	}
+
+	$('.close-btn').live('click',function(evt){
+		var closeBtn = $(evt.target);
+		var uploadId = closeBtn.attr('upload-id');
+		console.log(uploadId);
+		uploadList.removeItemById(uploadId);
+		closeBtn.parent().parent().get(0).removeChild(closeBtn.parent().get(0));
+	});
 });
 
 //TODO: a upload file list Object,contains add file, remove file function
@@ -67,37 +101,28 @@ UploadIdGen.prototype.gen = function() {
 
 // 上传列表
 function UploadItemList() {
-	this._itemArr = [];
+
 }
 
-UploadItemList.prototype.addItem = function (uploadItem) {
-	this._itmeArr.push(uploadItem);			
-	return this._itemArr.length -1;
-}
+UploadItemList.prototype = Array.prototype;
+
+UploadItemList.prototype.constructor = UploadItemList;
 
 UploadItemList.prototype.removeItem = function (uploadItem) {
-	var itemIndex = this._itmeArr.indexOf(uploadItem);
+	var itemIndex = this.indexOf(uploadItem);
 	if (itemIndex >= 0) {
-		this._itemArr.splice(itemIndex,1);
+		this.splice(itemIndex,1);
 	}
 	return itemIndex;
 }
 
 UploadItemList.prototype.removeItemById = function (uploadItemId) {
-	for(var i = 0; i < this._itemArr.length; i++) {
-		var item = this._itemArr[i];
+	for(var i = 0; i < this.length; i++) {
+		var item = this[i];
 		if(item.id == uploadItemId) {
-			this._itemArr.splice(i,1);
+			this.splice(i,1);
 			return i;
 		}
 	}
 	return -1;
-}
-
-UploadItemList.prototype.shift = function () {
-	return this._itmeArr.shift();	
-}
-
-UploadItemList.prototype.length = function () {
-	return this._itemArr.length;
 }
